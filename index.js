ca7d607322e9f28bf54c800b5e3fa6e7ac855fc5
SCARLET-SARAH <162133775+SCARLET-SARAH@users.noreply.github.com>
2024-03-23 22:53:04 +0300

    create loop.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+names =  [ 'Guadalupe', 'Ollie', 'Aki' ] 
+
+function writeCards(names) {
+  let messages = [];
+  names.forEach((name, index) => {
+    messages.push("Thank you, " + name + " at " + index + " for the wonderful gift");
+  });
+  return messages;
+}
+
+console.log(writeCards(names));
+  let arr = []
+  let realArray = [1,2,3,4,5] 
+  for (let index = 0; index < realArray.length; index++) { 
+     const element = realArray[index] 
+     arr.push(element)
+  }
+
+  console.log(arr)//your arr = 1,2,3,4,5
